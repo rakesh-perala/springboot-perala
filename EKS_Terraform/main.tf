4bf54d65348fab74c647a5e091d74701afcf9964
Rakesh Perala <raakeshperala16@gmail.com>
2026-03-11 20:30:30 +0530

    Update main.tf

--- EKS_Terraform/main.tf
+++ EKS_Terraform/main.tf
@@ -1,3 +1,4 @@
+
 data "aws_iam_policy_document" "assume_role" {
   statement {
     effect = "Allow"
@@ -34,7 +35,7 @@ data "aws_subnets" "public" {
 }
 #cluster provision
 resource "aws_eks_cluster" "example" {
-  name     = "EKS_CLOUD"
+  name     = "EKS_CLUSTER"
   role_arn = aws_iam_role.example.arn
 
   vpc_config {
