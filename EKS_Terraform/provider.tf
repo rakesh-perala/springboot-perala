a00b6424012dfa8d575a48929e2cd525e5db6806
Rakesh Perala <raakeshperala16@gmail.com>
2026-03-12 10:33:41 +0530

    Change AWS region and add default tags in provider.tf
    Updated AWS region and added default tags for resources.

--- EKS_Terraform/provider.tf
+++ EKS_Terraform/provider.tf
@@ -1,4 +1,6 @@
 terraform {
+  required_version = ">= 1.5"
+
   required_providers {
     aws = {
       source  = "hashicorp/aws"
@@ -9,5 +11,12 @@ terraform {
 
 # Configure the AWS Provider
 provider "aws" {
-  region = "us-east-1"
+  region = "ap-south-2"
+
+  default_tags {
+    tags = {
+      Project = "DevOps-EKS"
+      Owner   = "DevOpsFactory"
+    }
+  }
 }
