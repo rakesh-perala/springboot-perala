dd6047f40bee758178b63419ddbf1028c9b50ceb
Rakesh Perala <raakeshperala16@gmail.com>
2026-03-12 10:34:12 +0530

    Update S3 bucket and region in backend.tf

--- EKS_Terraform/backend.tf
+++ EKS_Terraform/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
-    bucket = "terraform-bucketrk" # Replace with your actual S3 bucket name
+    bucket = "devopsfactory-bk" # Replace with your actual S3 bucket name
     key    = "EKS/terraform.tfstate"
-    region = "us-east-1"
+    region = "ap-south-2"
   }
 }
